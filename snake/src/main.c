c82e4e9823dcc25c68cddf4ddd260151fdb22756
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-11-05 01:14:18 +1100

    snake: init window

--- snake/src/main.c
+++ snake/src/main.c
@@ -1,5 +1,34 @@
-#include <stdio.h>
+#include <ncurses.h>
 
-int main(int argc, char *argv[]) {
-  printf("Hello world!");
+int main(int argc, char **argv) {
+  int max_x = 0;
+  int max_y = 0;
+
+  // init screen and sets up screen
+  initscr();
+
+  // get max width and max height of the screen
+  getmaxyx(stdscr, max_y, max_x);
+
+  // create window
+  WINDOW *win = newwin(max_y - 2, max_x - 2, 1, 1);
+  refresh();
+
+  // make box border
+  box(win, 0, 0);
+
+  // move and print in window
+  mvwprintw(win, 0, 1, "Snake");
+  mvwprintw(win, 1, 1, "#####S @");
+
+  // refreshing the window
+  wrefresh(win);
+
+  // pause the screen output
+  getch();
+
+  // deallocates memory and ends ncurses
+  endwin();
+
+  return 0;
 }
