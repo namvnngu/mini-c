01fab516c2991d101208e93598b04894bc6f7884
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-11-27 01:48:56 +1100

    snake: improve ui and code

--- snake/src/main.c
+++ snake/src/main.c
@@ -1,16 +1,27 @@
 #include <ncurses.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
 
-#define DELAY_IN_MICROSECOND 100000
+#define DELAY_IN_MICROSECOND 50000
 
+#define APPLE "  "
+
+#define SNAKE "  "
 #define SNAKE_MAX_LENGTH 256
 
 struct vec2 {
   int x;
   int y;
+};
+
+enum direction {
+  NORTH,
+  EAST,
+  SOUTH,
+  WEST,
 };
 
 void setup(void);
@@ -25,21 +36,23 @@ int screen_height = 0;
 WINDOW *game_box;
 int game_box_width;
 int game_box_height;
-const char GAME_BOX_MARGIN_INLINE = 15;
-const char GAME_BOX_MARGIN_BLOCK = 5;
+const char GAME_BOX_MARGIN_INLINE = 50;
+const char GAME_BOX_MARGIN_BLOCK = 10;
 
 struct vec2 apple;
-const char *APPLE = "@";
+const int APPLE_COLOR_PAIR = 1;
+const int APPLE_OCCIPED_SPACES = strlen(APPLE);
 
-const char *SNAKE_HEAD = "x";
-const char *SNAKE_BODY = "*";
+const int SNAKE_COLOR_PAIR = 2;
 const int SNAKE_ABS_X_SPEED = 2;
 const int SNAKE_ABS_Y_SPEED = 1;
+const int SNAKE_OCCUPIED_SPACES = strlen(SNAKE);
 struct {
   struct vec2 positions[SNAKE_MAX_LENGTH];
   struct vec2 last_positions[SNAKE_MAX_LENGTH];
   struct vec2 speed;
   int length;
+  enum direction direction;
 } snake;
 
 bool over = false;
@@ -60,6 +73,11 @@ void setup(void) {
   keypad(stdscr, TRUE);
   nodelay(stdscr, TRUE);
 
+  start_color();
+  use_default_colors();
+  init_pair(SNAKE_COLOR_PAIR, -1, COLOR_WHITE);
+  init_pair(APPLE_COLOR_PAIR, -1, COLOR_RED);
+
   getmaxyx(stdscr, screen_height, screen_width);
   game_box_width = screen_width - GAME_BOX_MARGIN_INLINE * 2;
   game_box_height = screen_height - GAME_BOX_MARGIN_BLOCK * 2;
@@ -69,13 +87,12 @@ void setup(void) {
   apple.x = rand_int(4, game_box_width - 2);
   apple.y = rand_int(4, game_box_height - 2);
 
-  snake.length = 2;
-  snake.positions[0].x = 2;
+  snake.length = 1;
+  snake.positions[0].x = SNAKE_OCCUPIED_SPACES;
   snake.positions[0].y = 1;
-  snake.positions[1].x = 1;
-  snake.positions[1].y = 1;
-  snake.speed.x = 2;
+  snake.speed.x = SNAKE_ABS_X_SPEED;
   snake.speed.y = 0;
+  snake.direction = EAST;
 }
 
 void update(void) {
@@ -95,6 +112,7 @@ void update(void) {
         if (snake.speed.y == 0) {
           snake.speed.x = 0;
           snake.speed.y = -SNAKE_ABS_Y_SPEED;
+          snake.direction = NORTH;
         }
 
         break;
@@ -103,6 +121,7 @@ void update(void) {
         if (snake.speed.y == 0) {
           snake.speed.x = 0;
           snake.speed.y = SNAKE_ABS_Y_SPEED;
+          snake.direction = SOUTH;
         }
 
         break;
@@ -111,6 +130,7 @@ void update(void) {
         if (snake.speed.x == 0) {
           snake.speed.x = SNAKE_ABS_X_SPEED;
           snake.speed.y = 0;
+          snake.direction = EAST;
         }
 
         break;
@@ -119,6 +139,7 @@ void update(void) {
         if (snake.speed.x == 0) {
           snake.speed.x = -SNAKE_ABS_X_SPEED;
           snake.speed.y = 0;
+          snake.direction = WEST;
         }
 
         break;
@@ -151,7 +172,6 @@ void update(void) {
          apple.y < snake.positions[0].y + snake.speed.y) ||
         (snake.speed.y < 0 && snake.positions[0].y + snake.speed.y < apple.y &&
          apple.y <= snake.positions[0].y);
-
     if (is_x_collided && is_y_collided) {
       snake.positions[snake.length] = snake.positions[snake.length - 1];
       snake.length += 1;
@@ -172,17 +192,20 @@ void draw(void) {
   box(game_box, 0, 0);
 
   // snake
-  mvwprintw(game_box, snake.positions[0].y, snake.positions[0].x, SNAKE_HEAD);
-  for (int i = 1; i < snake.length; i++) {
+  wattron(game_box, COLOR_PAIR(SNAKE_COLOR_PAIR));
+  for (int i = 0; i < snake.length; i++) {
     int sx = snake.positions[i].x;
     int sy = snake.positions[i].y;
     if (sx != 0 && sy != 0) {
-      mvwprintw(game_box, sy, sx, SNAKE_BODY);
+      mvwprintw(game_box, sy, sx, SNAKE);
     }
   }
+  wattroff(game_box, COLOR_PAIR(SNAKE_COLOR_PAIR));
 
   // apple
-  mvwprintw(game_box, apple.y, apple.x, APPLE);
+  wattron(game_box, COLOR_PAIR(APPLE_COLOR_PAIR));
+  mvwprintw(game_box, apple.y, apple.x + APPLE_OCCIPED_SPACES, APPLE);
+  wattroff(game_box, COLOR_PAIR(APPLE_COLOR_PAIR));
 
   wrefresh(game_box);
 }
