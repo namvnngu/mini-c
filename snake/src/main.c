66e396aaef994bd8d6b9f9aae27144ef5bcf011d
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-12-08 17:36:41 +1100

    snake: add size2 struct

--- snake/src/main.c
+++ snake/src/main.c
@@ -6,10 +6,17 @@
 #include <unistd.h>
 
 #define DELAY_IN_MICROSECOND 75000
+#define CELL_WIDTH 2
+#define CELL_HEIGHT 1
 
 struct vec2 {
   int x;
   int y;
+};
+
+struct size2 {
+  int width;
+  int height;
 };
 
 enum direction {
@@ -19,13 +26,30 @@ enum direction {
   WEST,
 };
 
+struct {
+  struct size2 size;
+} screen;
+
 void game_setup(void);
 void game_loop(void);
 
+struct {
+  WINDOW *win;
+  struct vec2 position;
+  struct size2 size;
+  int border_width;
+} map;
 void map_init(void);
 void map_update(void);
 void map_draw(void);
 
+struct {
+  int parts_length;
+  struct vec2 *parts;
+  struct size2 part_size;
+  enum direction direction;
+  int color_pair_id;
+} snake;
 void snake_init(void);
 void snake_update(int key_input);
 bool snake_hit_wall(void);
@@ -33,38 +57,17 @@ bool snake_hit_itself(void);
 bool snake_hit_apple(void);
 void snake_draw(void);
 
+struct {
+  struct vec2 position;
+  struct size2 size;
+  int color_pair_id;
+} apple;
 void apple_init(void);
 void apple_update(void);
 void apple_draw(void);
 void apple_generate(void);
 
 int random_range(int min, int max);
-
-int screen_width = 0;
-int screen_height = 0;
-
-struct {
-  WINDOW *win;
-  struct vec2 position;
-  int width;
-  int height;
-  int border_width;
-} map;
-
-struct {
-  struct vec2 position;
-  char *presentation;
-  int color_pair_id;
-} apple;
-
-struct {
-  int length;
-  struct vec2 *parts;
-  char *single_part_presentation;
-  enum direction direction;
-  struct vec2 abs_speed;
-  int color_pair_id;
-} snake;
 
 int main(int argc, char **argv) {
   game_setup();
@@ -117,41 +120,42 @@ void game_loop(void) {
 }
 
 void map_init(void) {
-  getmaxyx(stdscr, screen_height, screen_width);
-  map.width = 50;
-  map.height = 25;
-  map.position.x = screen_width / 2 - map.width / 2;
-  map.position.y = screen_height / 2 - map.height / 2;
-  map.win = newwin(map.height, map.width, map.position.y, map.position.x);
+  getmaxyx(stdscr, screen.size.height, screen.size.width);
+  map.size.width = 50;
+  map.size.height = 25;
+  map.position.x = screen.size.width / 2 - map.size.width / 2;
+  map.position.y = screen.size.height / 2 - map.size.height / 2;
+  map.win =
+      newwin(map.size.height, map.size.width, map.position.y, map.position.x);
   map.border_width = 1;
 }
 void map_update(void) {
-  getmaxyx(stdscr, screen_height, screen_width);
+  getmaxyx(stdscr, screen.size.height, screen.size.width);
 }
 void map_draw(void) {
   box(map.win, 0, 0); // Drawer border
 }
 
 void snake_init(void) {
-  struct vec2 *parts = malloc(sizeof(struct vec2) * map.height * map.width);
+  struct vec2 *parts =
+      malloc(sizeof(struct vec2) * map.size.height * map.size.width);
   if (parts == NULL) {
     fprintf(stderr, "Out of memory.");
     exit(1);
   }
 
   snake.parts = parts;
-  snake.length = 1;
+  snake.parts_length = 1;
+  snake.part_size.width = 1;
+  snake.part_size.height = 1;
   snake.parts[0].x = 1;
   snake.parts[0].y = 1;
-  snake.single_part_presentation = "  ";
   snake.direction = EAST;
-  snake.abs_speed.x = strlen(snake.single_part_presentation);
-  snake.abs_speed.y = 1;
   snake.color_pair_id = 1;
   init_pair(snake.color_pair_id, -1, COLOR_BLUE);
 }
 void snake_update(int key_input) {
-  for (int i = snake.length - 1; i > 0; i--) {
+  for (int i = snake.parts_length - 1; i > 0; i--) {
     snake.parts[i] = snake.parts[i - 1];
   }
 
@@ -184,34 +188,34 @@ void snake_update(int key_input) {
 
   switch (snake.direction) {
     case NORTH: {
-      snake.parts[0].y -= snake.abs_speed.y;
+      snake.parts[0].y -= snake.part_size.height * CELL_HEIGHT;
       break;
     }
     case SOUTH: {
-      snake.parts[0].y += snake.abs_speed.y;
+      snake.parts[0].y += snake.part_size.height * CELL_HEIGHT;
       break;
     }
     case EAST: {
-      snake.parts[0].x += snake.abs_speed.x;
+      snake.parts[0].x += snake.part_size.width * CELL_WIDTH;
       break;
     }
     case WEST: {
-      snake.parts[0].x -= snake.abs_speed.x;
+      snake.parts[0].x -= snake.part_size.width * CELL_WIDTH;
       break;
     }
   }
 
   if (snake_hit_apple()) {
-    snake.length++;
+    snake.parts_length++;
     apple_generate();
   }
 }
 bool snake_hit_wall(void) {
-  return snake.parts[0].x <= 0 || snake.parts[0].x >= map.width - 1 ||
-         snake.parts[0].y <= 0 || snake.parts[0].y >= map.height - 1;
+  return snake.parts[0].x <= 0 || snake.parts[0].x >= map.size.width - 1 ||
+         snake.parts[0].y <= 0 || snake.parts[0].y >= map.size.height - 1;
 }
 bool snake_hit_itself(void) {
-  for (int i = 1; i < snake.length; i++) {
+  for (int i = 1; i < snake.parts_length; i++) {
     if (snake.parts[i].x == snake.parts[0].x &&
         snake.parts[i].y == snake.parts[0].y) {
       return true;
@@ -225,19 +229,20 @@ bool snake_hit_apple(void) {
 }
 void snake_draw(void) {
   wattron(map.win, COLOR_PAIR(snake.color_pair_id));
-  for (int i = 0; i < snake.length; i++) {
+  for (int i = 0; i < snake.parts_length; i++) {
     int sx = snake.parts[i].x;
     int sy = snake.parts[i].y;
     if (sx != 0 && sy != 0) {
-      mvwprintw(map.win, sy, sx, snake.single_part_presentation);
+      mvwprintw(map.win, sy, sx, "  ");
     }
   }
   wattroff(map.win, COLOR_PAIR(snake.color_pair_id));
 }
 
 void apple_init(void) {
-  apple.presentation = "  ";
   apple_generate();
+  apple.size.width = 1;
+  apple.size.height = 1;
   apple.color_pair_id = 2;
   init_pair(apple.color_pair_id, -1, COLOR_RED);
 }
@@ -245,31 +250,29 @@ void apple_update(void) {
 }
 void apple_draw(void) {
   wattron(map.win, COLOR_PAIR(apple.color_pair_id));
-  mvwprintw(map.win, apple.position.y, apple.position.x, apple.presentation);
+  mvwprintw(map.win, apple.position.y, apple.position.x, "  ");
   wattroff(map.win, COLOR_PAIR(apple.color_pair_id));
 }
 void apple_generate(void) {
   bool found = false;
   while (!found) {
-    int apple_width = strlen(apple.presentation);
-    int apple_height = 1;
-    apple.position.x =
-        random_range(1, map.width - map.border_width - apple_width);
-    apple.position.y =
-        random_range(1, map.height - map.border_width - apple_height);
+    apple.position.x = random_range(1, map.size.width - map.border_width -
+                                           apple.size.width * CELL_WIDTH);
+    apple.position.y = random_range(1, map.size.height - map.border_width -
+                                           apple.size.height * CELL_HEIGHT);
 
     if ((apple.position.x % 2) != (snake.parts[0].x % 2)) {
       continue;
     }
 
-    bool is_with_snake_body = false;
-    for (int i = 0; i < snake.length; i++) {
+    bool is_within_snake_body = false;
+    for (int i = 0; i < snake.parts_length; i++) {
       if (snake.parts[i].x == apple.position.x &&
           snake.parts[i].y == apple.position.y) {
-        is_with_snake_body = true;
+        is_within_snake_body = true;
       }
     }
-    if (is_with_snake_body) {
+    if (is_within_snake_body) {
       continue;
     }
 
