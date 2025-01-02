4cc3e1ee92113cb338c209fde979b911009b5f97
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-01-02 20:12:42 +1100

    feat(tetris): init ncurses

--- snake/src/main.c
+++ snake/src/main.c
@@ -29,5 +29,6 @@ int main(int argc, char *argv[]) {
   }
 
   endwin();
+
   return 0;
 }
--- tetris/src/main.c
+++ tetris/src/main.c
@@ -1,3 +1,21 @@
+#include <ncurses.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
 int main(int argc, char *argv[]) {
+  srand(time(NULL));
+
+  initscr();            // Initialize the screen
+  cbreak();             // Disable line buffering
+  noecho();             // Do not show typed characters
+  curs_set(false);      // Hide the cursor
+  keypad(stdscr, true); // Enable special keys like arrows
+  start_color();        // Enable colors (create colors and color pairs)
+  use_default_colors(); // Allow default terminal colors
+  refresh();            // Draw the screen
+
+  endwin()
+
   return 0;
 }
