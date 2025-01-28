61c17d2880f77e3fc577771f6d6b85c6f9f2b271
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-01-29 02:32:17 +1100

    feat(tetris): create entity stack

--- tetris/src/ecs.h
+++ tetris/src/ecs.h
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #define MAX_ENTITIES   300
@@ -7,6 +8,11 @@
 
 typedef uint32_t ecs_entity_t;
 typedef uint32_t ecs_component_t;
+
+struct ecs_entity_stack {
+  uint32_t data[MAX_ENTITIES];
+  int16_t top;
+};
 
 struct ecs_component_pool {
   void *data;
@@ -18,5 +24,28 @@ struct ecs_component_pool {
 struct ecs_world {
   uint32_t *entities;
   uint32_t *components;
-  ecs_component_pool *component_pools;
+  struct ecs_component_pool *component_pools;
 };
+
+static struct ecs_entity_stack ecs__entity_stack_new() {
+  struct ecs_entity_stack s;
+  for (size_t i = 0; i < MAX_ENTITIES; i++) {
+    s.data[i] = MAX_ENTITIES - i - 1;
+  }
+  s.top = MAX_ENTITIES - 1;
+  return s;
+}
+
+static uint32_t ecs__entity_stack_pop(struct ecs_entity_stack *s) {
+  if (s->top == -1) {
+    return MAX_ENTITIES;
+  }
+  return s->data[s->top--];
+}
+
+static void ecs__entity_stack_push(struct ecs_entity_stack *s, uint32_t e) {
+  if (s->top == MAX_ENTITIES - 1) {
+    return;
+  }
+  s->data[++s->top] = e;
+}
