ac8154a74d96ee6fde58d3ad021a0164d0ca40e3
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-04-16 00:46:08 +1000

    fizzbuzz: first draft

--- fizzbuzz/main.c
+++ fizzbuzz/main.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
 
+void fizzbuzz(char buf[], int bufsize, int upto) {
+  int formatted_size = 0;
+
+  for (int index = 1; index <= upto; index++) {
+    if (index % 15 == 0) {
+      formatted_size += snprintf(buf + formatted_size,
+                                 bufsize - formatted_size,
+                                 index == upto ? "%s." : "%s, ",
+                                 "FizzBuzz");
+    } else if (index % 3 == 0) {
+      formatted_size += snprintf(buf + formatted_size,
+                                 bufsize - formatted_size,
+                                 index == upto ? "%s." : "%s, ",
+                                 "Fizz");
+    } else if (index % 5 == 0) {
+      formatted_size += snprintf(buf + formatted_size,
+                                 bufsize - formatted_size,
+                                 index == upto ? "%s." : "%s, ",
+                                 "Buzz");
+    } else {
+      formatted_size += snprintf(buf + formatted_size,
+                                 bufsize - formatted_size,
+                                 index == upto ? "%d." : "%d, ",
+                                 index);
+    }
+  }
+}
+
 int main(void) {
-  printf("Hello world!\n");
+  int BUFSIZE = 10000;
+  char buf[BUFSIZE];
+  fizzbuzz(buf, BUFSIZE, 100);
+  printf("%s\n", buf);
 }
