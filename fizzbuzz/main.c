4ee8e95adfefc33ffb0b60828d78d774c2bcf4b7
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-04-16 18:22:23 +1000

    fizzbuzz: update idx to i

--- fizzbuzz/main.c
+++ fizzbuzz/main.c
@@ -3,19 +3,19 @@
 void fizzbuzz(char buf[], int bufsize, int upto) {
   int fmtsize = 0;
 
-  for (int idx = 1; idx <= upto; idx++) {
+  for (int i = 1; i <= upto; i++) {
     char *str = buf + fmtsize;
     int restsize = bufsize - fmtsize;
-    int islast = idx == upto;
+    int islast = i == upto;
 
-    if (idx % 15 == 0) {
+    if (i % 15 == 0) {
       fmtsize += snprintf(str, restsize, islast ? "%s." : "%s, ", "FizzBuzz");
-    } else if (idx % 3 == 0) {
+    } else if (i % 3 == 0) {
       fmtsize += snprintf(str, restsize, islast ? "%s." : "%s, ", "Fizz");
-    } else if (idx % 5 == 0) {
+    } else if (i % 5 == 0) {
       fmtsize += snprintf(str, restsize, islast ? "%s." : "%s, ", "Buzz");
     } else {
-      fmtsize += snprintf(str, restsize, islast ? "%d." : "%d, ", idx);
+      fmtsize += snprintf(str, restsize, islast ? "%d." : "%d, ", i);
     }
   }
 }
