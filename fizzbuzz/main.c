705ac120482fb0d6fc2568e05def6ac9c8713a2d
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-04-16 17:12:36 +1000

    fizzbuzz: finish

--- fizzbuzz/main.c
+++ fizzbuzz/main.c
@@ -1,36 +1,27 @@
 #include <stdio.h>
 
 void fizzbuzz(char buf[], int bufsize, int upto) {
-  int formatted_size = 0;
+  int fmtsize = 0;
 
-  for (int index = 1; index <= upto; index++) {
-    if (index % 15 == 0) {
-      formatted_size += snprintf(buf + formatted_size,
-                                 bufsize - formatted_size,
-                                 index == upto ? "%s." : "%s, ",
-                                 "FizzBuzz");
-    } else if (index % 3 == 0) {
-      formatted_size += snprintf(buf + formatted_size,
-                                 bufsize - formatted_size,
-                                 index == upto ? "%s." : "%s, ",
-                                 "Fizz");
-    } else if (index % 5 == 0) {
-      formatted_size += snprintf(buf + formatted_size,
-                                 bufsize - formatted_size,
-                                 index == upto ? "%s." : "%s, ",
-                                 "Buzz");
+  for (int idx = 1; idx <= upto; idx++) {
+    char *str = buf + fmtsize;
+    int restsize = bufsize - fmtsize;
+    int islast = idx == upto;
+
+    if (idx % 15 == 0) {
+      fmtsize += snprintf(str, restsize, islast ? "%s." : "%s, ", "FizzBuzz");
+    } else if (idx % 3 == 0) {
+      fmtsize += snprintf(str, restsize, islast ? "%s." : "%s, ", "Fizz");
+    } else if (idx % 5 == 0) {
+      fmtsize += snprintf(str, restsize, islast ? "%s." : "%s, ", "Buzz");
     } else {
-      formatted_size += snprintf(buf + formatted_size,
-                                 bufsize - formatted_size,
-                                 index == upto ? "%d." : "%d, ",
-                                 index);
+      fmtsize += snprintf(str, restsize, islast ? "%d." : "%d, ", idx);
     }
   }
 }
 
 int main(void) {
-  int BUFSIZE = 10000;
-  char buf[BUFSIZE];
-  fizzbuzz(buf, BUFSIZE, 100);
+  char buf[1000];
+  fizzbuzz(buf, sizeof buf, 100);
   printf("%s\n", buf);
 }
